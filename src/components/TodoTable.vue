abd62ae98cdbc0efe9ce871635c57dd77391309c
Danila Anikin <danila.s.anikin@gmail.com>
2023-01-16 20:19:18 +0100

    completed

--- src/components/TodoTable.vue
+++ src/components/TodoTable.vue
@@ -2,40 +2,66 @@
 import {supabase} from '../supabase'
 import {ref, onMounted} from 'vue'
 
-const allData = ref([])
-const finish = ref()
+const todos = ref([])
+const addTodo = ref("")
+const dropdown = ref(false)
+const noText = ref(false)
 
 const getData = async() => {
   const {data} = await supabase.from('ToDos').select();
   
-  allData.value = data
+  todos.value = data
 }
 
 const changeStatus = async(id, status) => {
   const {changeStatus} = await supabase.from('ToDos').update({Status: !status}).eq('id', id);
-  const {data} = await supabase.from('ToDos').select();
-  allData.value = data
+  getData()
+}
+
+const addToList = async() => {
+  if(addTodo.value !== ""){
+    noText.value = false
+    const {addToList} = await supabase.from('ToDos').insert({id: todos.value.length+1, WhatToDo: addTodo.value, Status: false})
+    dropdown.value = false
+    addTodo.value = ""
+    getData()
+  } else{
+    noText.value = true
+  }
+}
+
+const deleteTodo = async(id) => {
+  const {deleteTodo} = await supabase.from('ToDos').delete().eq('id', id)
+  getData()
 }
 
 onMounted(getData)
 
 </script>
-
 <template>
   <div class="mx-auto bg-blue-700 p-10 mt-16 max-w-xl w-full h-32 rounded-2xl shadow-xl shadow-blue-800 text-center text-slate-200 text-3xl font-bold">
     <span>To Do List</span>
   </div>
-  <div class="m-auto bg-blue-700 p-10 mt-12 max-w-xl w-full rounded-2xl shadow-2xl shadow-blue-600">
-    <div class="flex justify-between py-4 px-8 bg-blue-500 text-slate-100 font-bold rounded-xl text-lg shadow-xl shadow-blue-700">
-      <span>Task</span>
-      <span>Until when</span>
+  <div class="m-auto bg-blue-700 p-10 mt-12 max-w-xl w-full rounded-2xl">
+    <div class="flex justify-center py-4 px-8 bg-blue-500 text-slate-100 font-bold rounded-xl text-lg">
+      <span class="-ml-32 mr-40">Task</span>
       <span>Status</span>
     </div>
-    <div v-for="list in allData" :class="`flex justify-between text-lg shadow-xl shadow-blue-900 rounded-2xl p-6 mx-auto mt-8 ${list.Status ? 'bg-green-400 text-green-900' : 'bg-blue-300 text-blue-600'} max-w-lg w-full h-20`">
-      <span> {{ list.WhatToDo }} </span>
-      <span> {{ list.UntilWhen }} </span>
-      <button @click="changeStatus(list.id, list.Status)" :class="`text-slate-100 p-2 rounded-lg w-20 h-10 -mt-1 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-if="list.Status">Finished</button>
-      <button @click="changeStatus(list.id, list.Status)" :class="`text-slate-100 p-2 rounded-lg w-20 h-10 -mt-1 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-else>Finish</button>
+    <div v-for="list in todos" :class="`flex justify-center text-lg rounded-2xl p-6 mx-auto mt-8 ${list.Status ? 'bg-green-400 text-green-900' : 'bg-blue-300 text-blue-600'} max-w-lg w-full`">
+      <span class="w-40"> {{ list.WhatToDo }} </span>
+      <button @click="changeStatus(list.id, list.Status)" :class="`mr-8 ml-4 text-slate-100 p-2 rounded-lg w-32 h-10 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-if="list.Status">Finished</button>
+      <button @click="changeStatus(list.id, list.Status)" :class="`mr-8 ml-4 text-slate-100 p-2 rounded-lg w-32 h-10 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-else>In process</button>
+      <button @click="deleteTodo(list.id)" class="text-slate-100 p-2 rounded-lg w-20 h-10 bg-red-700">Delete</button>
     </div>
+
+    <div class="mx-auto mt-9 text-center">
+      <button @click="dropdown = !dropdown, addTodo=''" class="bg-transparent text-slate-200 border-2 border-slate-300 font-bold max-w-xs w-full h-16 text-xl rounded-2xl">
+        New
+      </button>
+      <input v-if="dropdown" :class="`${noText ? 'bg-red-300 placeholder:text-red-700' : 'bg-blue-100 placeholder:text-slate-500'} p-3 w-64 mt-4 rounded-xl focus:bg-blue-200 focus:placeholder:text-slate-500 outline-none`" placeholder="What to do?" v-model="addTodo">
+      <button v-if="dropdown" class="bg-slate-200 block w-32 h-10 text-blue-600 font-bold text-xl rounded-xl mx-auto mt-2 focus:bg-blue-300" @click="addToList">Add</button>
+    </div>
+    
   </div>
+
 </template>
