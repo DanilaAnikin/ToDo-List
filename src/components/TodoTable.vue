fba28de9784404a9b44e61294fbc877eed48482f
Danila Anikin <danila.s.anikin@gmail.com>
2023-02-07 17:06:42 +0100

    some style?

--- src/components/TodoTable.vue
+++ src/components/TodoTable.vue
@@ -2,7 +2,8 @@
 import {supabase} from '../supabase'
 import {ref, onMounted} from 'vue'
 import {TrashIcon} from '@heroicons/vue/24/outline'
-import {CheckCircleIcon} from '@heroicons/vue/24/outline';
+import {CheckCircleIcon} from '@heroicons/vue/24/solid';
+import {ArrowTrendingUpIcon} from '@heroicons/vue/24/solid';
 
 
 const todos = ref([])
@@ -42,33 +43,33 @@ onMounted(getData)
 
 </script>
 <template>
-  <div class="mx-auto bg-blue-700 p-10 mt-16 max-w-xl w-full h-32 rounded-2xl shadow-xl shadow-blue-800 text-center text-slate-200 text-3xl font-bold">
+  <div class="mx-auto bg-transparent p-10 mt-16 max-w-xl w-full h-32 rounded-2xl text-center text-slate-800 border-2 border-slate-600 text-3xl font-bold">
     <span>To Do List</span>
   </div>
-  <div class="m-auto bg-blue-700 p-10 mt-12 max-w-xl w-full rounded-2xl">
-    <div v-if="todos.length != 0" class="flex justify-center py-4 px-8 bg-blue-500 text-slate-100 font-bold rounded-xl text-lg">
-      <span class="-ml-28 mr-44">Task</span>
+  <div class="m-auto bg-transparent p-10 mt-12 max-w-xl w-full rounded-2xl border border-slate-800">
+    <div v-if="todos.length != 0" class="flex justify-center py-4 px-8 bg-transparent text-slate-900 font-bold rounded-xl text-lg">
+      <span class="-ml-36 mr-44">Task</span>
       <span>Status</span>
     </div>
-    <div v-for="list in todos" :class="`flex justify-center text-lg rounded-2xl p-6 mx-auto mt-8 ${list.Status ? 'bg-green-400 text-green-900' : 'bg-blue-300 text-blue-600'} max-w-lg w-full`">
-      <span class="w-40"> {{ list.WhatToDo }} </span>
-      <button @click="changeStatus(list.id, list.Status)" :class="`mr-20 ml-14 text-slate-100 p-2 rounded-lg w-10 h-10 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-if="list.Status">
-        <CheckCircleIcon class="w-6 h-6 text-slate-100"/>
+    <div v-for="list in todos" :class="`flex justify-center text-lg rounded-2xl p-6 mx-auto mt-6 items-center ${list.Status ? 'bg-slate-300' : 'bg-transparent'} border border-slate-800 text-slate-900 max-w-lg w-full`">
+      <span class="w-40 -ml-8"> {{ list.WhatToDo }} </span>
+      <button @click="changeStatus(list.id, list.Status)" class="mr-20 ml-16 text-slate-100 bg-transparent justify-center flex items-center rounded-lg w-10 h-10" v-if="list.Status">
+        <CheckCircleIcon class="w-9 h-9 text-green-700"/>
       </button>
-      <button @click="changeStatus(list.id, list.Status)" :class="`mr-8 ml-4 text-slate-100 p-2 rounded-lg w-32 h-10 ${list.Status ? 'bg-green-700' : 'bg-blue-600'}`" v-else>
-        In process
+      <button @click="changeStatus(list.id, list.Status)" class="mr-20 ml-16 text-slate-900 bg-transparent rounded-lg w-10 h-10" v-else>
+        <ArrowTrendingUpIcon class="w-9 h-9 text-slate-800"/>
       </button>
-      <button @click="deleteTodo(list.id)" class="text-slate-100 p-2 rounded-lg w-10 h-10 bg-red-700">
-        <TrashIcon class="h-6 w-5 text-slate-200 m-auto"/>
+      <button @click="deleteTodo(list.id)" class="text-red-700 hover:text-red-600 hover:border-red-600 border-2 border-red-700 rounded-lg w-10 h-10 bg-transparent">
+        <TrashIcon class="h-6 w-5 m-auto"/>
       </button>
     </div>
 
     <div class="mx-auto mt-9 text-center">
-      <button @click="dropdown = !dropdown, addTodo=''" class="bg-transparent text-slate-200 border-2 border-slate-300 font-bold max-w-xs w-full h-16 text-xl rounded-2xl">
+      <button @click="dropdown = !dropdown, addTodo='', noText=false" class="hover:bg-slate-200 bg-transparent text-slate-800 border-2 border-slate-600 font-bold max-w-xs w-full h-16 text-xl rounded-2xl">
         New
       </button>
-      <input v-if="dropdown" :class="`${noText ? 'bg-red-300 placeholder:text-red-700' : 'bg-blue-100 placeholder:text-slate-500'} p-3 w-64 mt-4 rounded-xl focus:bg-blue-200 focus:placeholder:text-slate-500 outline-none`" placeholder="What to do?" v-model="addTodo">
-      <button v-if="dropdown" class="bg-slate-200 block w-32 h-10 text-blue-600 font-bold text-xl rounded-xl mx-auto mt-2 focus:bg-blue-300" @click="addToList">Add</button>
+      <input v-if="dropdown" v-on:keyup.enter="addToList" :class="`${noText ? 'bg-red-100 placeholder:text-red-400' : 'bg-transparent placeholder:text-slate-500'} border border-slate-500 p-3 w-64 mt-4 rounded-xl focus:bg-slate-200 focus:placeholder:text-slate-500 outline-none`" placeholder="What to do?" v-model="addTodo">
+      <button v-if="dropdown" class="bg-transparent block w-24 h-10 text-slate-800 font-bold border border-slate-800 text-xl rounded-xl mx-auto mt-2 focus:bg-slate-300" @click="addToList">Add</button>
     </div>
     
   </div>
@@ -77,6 +78,6 @@ onMounted(getData)
 
 <style>
 body{
-  background-color: lightblue;
+  background-color: rgb(233, 235, 241);
 }
 </style>
